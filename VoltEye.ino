d49d4413d6b3808398dbf97ee25b1b3ddc421845
Matth-P <matheuspatriotaandrade@gmail.com>
2026-07-21 19:26:46 -0300

    Código revisado

--- VoltEye.ino
+++ VoltEye.ino
@@ -1,43 +1,20 @@
+#include <Arduino.h>
+#include <math.h>
+
 #include <WiFi.h>
 #include <HTTPClient.h>
-#include <math.h>
+
 #include "secrets.h"
 
-// ==========================
-// WIFI
-// ==========================
-
-const char* ssid = WIFI_SSID;
-const char* password = WIFI_PASSWORD;
-
-// ==========================
-// SUPABASE
-// ==========================
-
-const char* supabaseUrl =
-  SUPABASE_URL;
-
-const char* supabaseKey =
-  SUPABASE_KEY;
 
 // ==========================
 // PINOS
 // ==========================
 
-// ACS712
 const int pinoCorrente = 33;
-// Divisor resistivo
 const int pinoTensao = 32;
 
-// LED PWM
-const int ledEletrodomestico = 25;
 
-// ==========================
-// PWM
-// ==========================
-
-const int frequenciaPWM = 5000;
-const int resolucaoPWM = 8;
 
 // ==========================
 // ADC
@@ -46,273 +23,470 @@ const int resolucaoPWM = 8;
 const float VREF = 3.3;
 const int ADC_RESOLUTION = 4095;
 
-// ==========================
-// ACS712
-// ==========================
 
-// 5A = 185mV/A
-const float sensibilidadeACS = 0.185;
-
-// Offset inicial
-float offsetCorrente = 2.5;
 
 // ==========================
-// SETUP
+// ACS712 COM DIVISOR
+// 1k + 2k
 // ==========================
 
-void setup() {
+const float sensibilidadeACS = 0.066;
 
-  Serial.begin(115200);
+float offsetADC = 0;
 
-  delay(1000);
 
-  Serial.println("Iniciando ESP32...");
 
-  analogSetAttenuation(ADC_11db);
+// ==========================
+// ZMPT101B
+// divisor 1k/2k
+// ==========================
 
-  ledcAttach(
-    ledEletrodomestico,
-    frequenciaPWM,
-    resolucaoPWM
+float fatorTensao = 607.0;
+
+
+
+// ==========================
+// WIFI
+// ==========================
+
+void conectarWiFi(){
+
+
+  WiFi.begin(
+    WIFI_SSID,
+    WIFI_PASSWORD
   );
 
-  // ==========================
-  // WIFI
-  // ==========================
 
-  WiFi.begin(ssid, password);
+  Serial.print("Conectando WiFi");
 
-  while (WiFi.status() != WL_CONNECTED) {
+
+  while(WiFi.status() != WL_CONNECTED){
+
 
     delay(500);
 
-    Serial.println("Conectando WiFi...");
+    Serial.print(".");
+
   }
+
+
+  Serial.println();
 
   Serial.println("WiFi conectado!");
 
-  // ==========================
-  // CALIBRAÇÃO ACS712
-  // ==========================
-
-  long soma = 0;
-
-  for (int i = 0; i < 1000; i++) {
-
-    soma += analogRead(pinoCorrente);
-
-    delay(1);
-  }
-
-  float mediaADC = soma / 1000.0;
-
-  offsetCorrente =
-    (mediaADC * VREF) / ADC_RESOLUTION;
-
-  Serial.print("Offset calibrado: ");
-
-  Serial.println(offsetCorrente, 3);
 }
 
 
 
-void loop() {
+// ==========================
+// ENVIO SUPABASE
+// ==========================
 
-  // ==========================
-  // LEITURA CORRENTE
-  // ==========================
+void enviarDados(
+  float corrente,
+  float tensao,
+  float potencia
+){
 
-  float somaQuadrados = 0;
+  if(WiFi.status() == WL_CONNECTED){
 
-  int amostras = 1000;
-
-  for (int i = 0; i < amostras; i++) {
-
-    int leituraADC =
-      analogRead(pinoCorrente);
-
-    float tensaoSensor =
-      (leituraADC * VREF)
-      / ADC_RESOLUTION;
-
-    float corrente =
-      (tensaoSensor - offsetCorrente)
-      / sensibilidadeACS;
-
-    somaQuadrados +=
-      corrente * corrente;
-
-    delayMicroseconds(200);
-  }
-
-  float correnteRMS =
-    sqrt(
-      somaQuadrados
-      / amostras
-    );
-
-  // ==========================
-  // LEITURA TENSAO
-  // ==========================
-
-  int leituraTensao =
-    analogRead(pinoTensao);
-
-  float tensaoADC =
-    (leituraTensao * VREF)
-    / ADC_RESOLUTION;
-
-  float tensaoReal =
-    tensaoADC * 127.0;
-
-  float potencia =
-    tensaoReal * correnteRMS;
-
-  // ==========================
-  // LED PWM
-  // ==========================
-
-  int brilho = map(
-    (int)potencia,
-    0,
-    2500,
-    0,
-    255
-  );
-
-  brilho = constrain(
-    brilho,
-    0,
-    255
-  );
-
-  ledcWrite(
-    ledEletrodomestico,
-    brilho
-  );
-
-  // ==========================
-  // ALERTAS
-  // ==========================
-
-  if (correnteRMS < 0.2) {
-
-    Serial.println(
-      "CORRENTE BAIXA!"
-    );
-  }
-
-  if (tensaoReal < 200) {
-
-    Serial.println(
-      "QUEDA DE TENSAO!"
-    );
-  }
-
-  // ==========================
-  // SERIAL
-  // ==========================
-
-  Serial.println(
-    "========================"
-  );
-
-  Serial.print("Tensao: ");
-
-  Serial.print(
-    tensaoReal,
-    2
-  );
-
-  Serial.println(" V");
-
-  Serial.print("Corrente: ");
-
-  Serial.print(
-    correnteRMS,
-    2
-  );
-
-  Serial.println(" A");
-
-  Serial.print("Potencia: ");
-
-  Serial.print(
-    potencia,
-    2
-  );
-
-  Serial.println(" W");
-
-  // ==========================
-  // ENVIO SUPABASE
-  // ==========================
-
-  if (WiFi.status() == WL_CONNECTED) {
 
     HTTPClient http;
 
-    http.begin(
-      supabaseUrl
+
+    http.begin(API_URL);
+
+
+    http.addHeader(
+      "apikey",
+      SUPABASE_KEY
     );
+
+
+    http.addHeader(
+      "Authorization",
+      "Bearer " SUPABASE_KEY
+    );
+
 
     http.addHeader(
       "Content-Type",
       "application/json"
     );
 
-    http.addHeader(
-      "apikey",
-      supabaseKey
-    );
 
-    http.addHeader(
-      "Authorization",
-      String("Bearer ")
-      + supabaseKey
-    );
 
     String json = "{";
 
-    json += "\"corrente\":";
+
+    json += "\"device_id\":\"";
+    json += DEVICE_ID;
+    json += "\",";
+
+
+    json += "\"voltage\":";
+
     json += String(
-      correnteRMS,
+      tensao,
       2
     );
+
 
     json += ",";
 
-    json += "\"tensao\":";
+
+    json += "\"current\":";
+
     json += String(
-      tensaoReal,
+      corrente,
       2
     );
 
+
     json += "}";
 
-    Serial.println(
-      "JSON enviado:"
-    );
+
+
+    Serial.println("JSON enviado:");
 
     Serial.println(json);
 
-    int response =
-      http.POST(json);
 
-    Serial.print(
-      "HTTP Response: "
+
+    int resposta = http.POST(json);
+
+
+
+    Serial.print("HTTP:");
+
+    Serial.println(resposta);
+
+
+
+    Serial.println(
+      http.getString()
     );
 
-    Serial.println(response);
+
 
     http.end();
 
-  } else {
-
-    Serial.println(
-      "WiFi desconectado!"
-    );
   }
 
-  delay(5000);
+}
+
+
+
+
+void setup(){
+
+
+  Serial.begin(115200);
+
+
+  delay(1000);
+
+
+  analogSetAttenuation(ADC_11db);
+
+
+
+  conectarWiFi();
+
+
+
+  // ==========================
+  // CALIBRA ACS712
+  // ==========================
+
+
+  Serial.println("Calibrando ACS712...");
+
+
+
+  long soma = 0;
+
+
+
+  for(int i = 0; i < 10000; i++){
+
+
+    soma += analogRead(pinoCorrente);
+
+
+
+    delayMicroseconds(500);
+
+  }
+
+
+
+  offsetADC =
+  soma / 10000.0;
+
+
+
+  Serial.print("Offset ACS ADC: ");
+
+  Serial.println(offsetADC);
+
+
+
+}
+
+
+
+
+
+
+
+
+void loop(){
+
+
+  int amostras = 3000;
+
+
+
+  // ==========================
+  // CORRENTE RMS ACS712
+  // ==========================
+
+
+  float somaCorrente = 0;
+
+
+
+  for(int i = 0; i < amostras; i++){
+
+
+
+    int leitura =
+    analogRead(pinoCorrente);
+
+
+
+    float diferencaADC =
+    leitura - offsetADC;
+
+
+
+    if(abs(diferencaADC) < 20){
+
+      diferencaADC = 0;
+
+    }
+
+
+
+    float tensao =
+    (diferencaADC * VREF)
+    /
+    ADC_RESOLUTION;
+
+
+
+    float corrente =
+    tensao / sensibilidadeACS;
+
+
+
+    somaCorrente += corrente*corrente;
+
+
+
+    delayMicroseconds(200);
+
+
+  }
+
+
+
+
+
+  float correnteRMS =
+  sqrt(
+    somaCorrente/amostras
+  );
+
+
+
+  if(correnteRMS < 1.0){
+
+    correnteRMS = 0;
+
+  }
+
+
+
+
+
+
+
+
+  // ==========================
+  // TENSAO RMS ZMPT101B
+  // ==========================
+
+
+  float somaADC = 0;
+
+
+
+  for(int i=0;i<amostras;i++){
+
+
+    somaADC += analogRead(pinoTensao);
+
+
+
+    delayMicroseconds(200);
+
+  }
+
+
+
+
+  float mediaADC =
+  somaADC/amostras;
+
+
+
+
+
+
+  float somaQuadrados = 0;
+
+
+
+  for(int i=0;i<amostras;i++){
+
+
+
+    float leitura =
+    analogRead(pinoTensao);
+
+
+
+    float diferenca =
+    leitura-mediaADC;
+
+
+
+    somaQuadrados +=
+    diferenca*diferenca;
+
+
+
+    delayMicroseconds(200);
+
+  }
+
+
+
+
+  float rmsADC =
+  sqrt(
+    somaQuadrados/amostras
+  );
+
+
+
+
+
+  float tensaoReal =
+  (
+    rmsADC *
+    VREF /
+    ADC_RESOLUTION
+  )
+  *
+  fatorTensao;
+
+
+
+  // filtro ruído
+
+  if(tensaoReal < 50){
+
+    tensaoReal = 0;
+
+  }
+
+
+
+
+
+
+
+  // ==========================
+  // POTENCIA
+  // ==========================
+
+
+  float potencia =
+  tensaoReal * correnteRMS;
+
+
+
+
+
+
+
+  // ==========================
+  // SERIAL
+  // ==========================
+
+
+  Serial.println("===================");
+
+
+
+  Serial.print("Tensao: ");
+
+  Serial.print(tensaoReal,2);
+
+  Serial.println(" V");
+
+
+
+  Serial.print("Corrente: ");
+
+  Serial.print(correnteRMS,2);
+
+  Serial.println(" A");
+
+
+
+  Serial.print("Potencia: ");
+
+  Serial.print(potencia,2);
+
+  Serial.println(" W");
+
+
+
+  Serial.print("ADC ZMPT: ");
+
+  Serial.println(
+    analogRead(pinoTensao)
+  );
+
+
+
+
+  // ENVIA PARA API
+
+  enviarDados(
+    correnteRMS,
+    tensaoReal,
+    potencia
+  );
+
+
+
+  delay(10000);
+
+
 }
